5cb2452d109d85b1e20cd387df597c4c65de1adc
ManeaValentinCornel <manea.valentin.cornel@gmail.com>
2023-04-08 21:34:40 +0100

    Startup Server - EXPRESS

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require('express');
 
+//CREATE SERVER
+const server = express();
+
+//STARTUP SERVER WITHT HE PORT OF 3000
+const SERVER_PORT = 3000;
+server.listen(SERVER_PORT, () => {
+  console.log('Server running');
+});

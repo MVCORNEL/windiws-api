91e71e33b0759667dc9753d0a28e83baebcab818
ManeaValentinCornel <manea.valentin.cornel@gmail.com>
2023-04-16 20:30:24 +0100

    Factory habdler for CRUD operations for all Model/Documents

---
+++ controller/factoryCRUDHandler.js
@@ -0,0 +1,123 @@
+//Code inspired from https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/
+const catchAsync = require('./catchAsync');
+const FilterApi = require('../utils/filterAPI');
+const OperationalError = require('./../utils/operationalError');
+
+/**
+ * Middleware function used query the database for a document based on a provided _id and its data Model object,
+ * Sends the result back to the client.
+ * @function catchAsync used to catch and propagate further any occurring error
+ * @param {object} Model expects a Mongoose Model built on top a db schema.
+ * @param {object} req expects a request object
+ * @param {object} res expects a response object
+ * @param {function} next expects a function that will be used to  navigate to the next middleware
+ */
+exports.getDocument = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    //query the db for the entry based in the given _id
+    const document = await Model.findById({ _id: req.params.id });
+    //the query has not returned anything
+    if (!document) {
+      next(new OperationalError('No such document with found, wrong ID'));
+    }
+    //user feedback
+    res.status(200).json({
+      status: 'success',
+      data: { document },
+    });
+  });
+};
+
+/**
+ * Middleware function used query the database for a documents based on a provided Model data object,
+ * Sends the result back to the client.
+ * @class Filter Api class that will allow users to filter data by passing query string into the url like. (Filtering,Sorting,Projecting,Pagonating).
+ * @function catchAsync used to catch and propagate further any occurring error.
+ * @param {object} Model expects a Mongoose Model built on top a db schema.
+ * @param {object} req expects a request object
+ * @param {object} res expects a response object
+ * @param {function} next expects a function that will be used to  navigate to the next middleware
+ */
+exports.getAllDocuments = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    //query parameters coming from the client as a requset object on the body
+    const expressQuery = { ...req.query };
+    //mongoose query promise used to build up all the api features.
+    const mongooseQuery = Model.find();
+    const filteredQuery = new FilterApi(mongooseQuery, expressQuery).filter().sort().project().paginate();
+    //filtered,sorted,projected, and paginated results
+    const resultDocuments = await filteredQuery.mongooseQuery;
+    FilterApi;
+    res.status(200).json({
+      status: 'success',
+      results: resultDocuments.length,
+      data: {
+        resultDocuments,
+      },
+    });
+  });
+};
+
+/**
+ * Middleware function used to create a new document entry based on the user information passed into the body request object  by the user.
+ * Sends the result back to the client.
+ * @function catchAsync used to catch and propagate further any occurring error.
+ * @param {object} Model expects a Mongoose Model built on top a db schema.
+ * @param {object} req expects a request object
+ * @param {object} res expects a response object
+ * @param {function} next expects a function that will be used to  navigate to the next middleware
+ */
+exports.createDocument = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    //1 Create and insert into the Db de mongodb document
+    const document = await Model.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: { document },
+    });
+  });
+};
+
+/**
+ * Middleware function used to delete  a document entry based on a user id  passed into the body request object by the user.
+ * Sends the result back to the client.
+ * @function catchAsync used to catch and propagate further any occurring error.
+ * @param {object} Model expects a Mongoose Model built on top a db schema.
+ * @param {object} req expects a request object
+ * @param {object} res expects a response object
+ * @param {function} next expects a function that will be used to  navigate to the next middleware
+ */
+exports.deleteDocument = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const document = await Model.findOneAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: document,
+    });
+  });
+};
+
+/*
+ * Middleware function used to update  a document entry based on a user id  and its field passed into the request body by the user.
+ * Sends the result back to the client.
+ * @function catchAsync used to catch and propagate further any occurring error.
+ * @param {object} Model expects a Mongoose Model built on top a db schema.
+ * @param {object} req expects a request object
+ * @param {object} res expects a response object
+ * @param {function} next expects a function that will be used to  navigate to the next middleware
+ */
+exports.updateDocument = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const document = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      //returned document will be the newly updated one
+      new: true,
+      //each time a document is updated, the validators within the schema will run again
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { document },
+    });
+  });
+};
--- controller/productHandler.js
+++ controller/productHandler.js
@@ -1,86 +1,29 @@
-const catchAsync = require('./catchAsync');
 const Product = require('./../model/porductModel');
-const FilterApi = require('./../utils/filterAPI');
+const {
+  createDocument,
+  getDocument,
+  getAllDocuments,
+  deleteDocument,
+  updateDocument,
+} = require('./factoryCRUDHandler');
 
-exports.getProduct = catchAsync(async (req, res, next) => {
-  const product = await Product.findOne({ _id: req.params.id });
-
-  res.status(200).json({
-    status: 'success',
-    data: { product },
-  });
-});
+exports.getProduct = getDocument(Product);
 
 /**
  * Middleware function used query the database for produts, and send the respomnse as JSON to the client
- * @param {object} req expects a request object
- * @param {object} res expects a response object
- * @param {function} next expects a function that will be used to  navigate to the next middleware
  */
-exports.getAllProducts = catchAsync(async (req, res, next) => {
-  const expressQuery = { ...req.query };
-  console.log(expressQuery);
-  const mongooseQuery = Product.find();
-
-  const filteredQuery = new FilterApi(mongooseQuery, expressQuery).filter().sort().project().paginate();
-  const resultDocuments = await filteredQuery.mongooseQuery;
-  FilterApi;
-  res.status(200).json({
-    status: 'success',
-    results: resultDocuments.length,
-    data: {
-      resultDocuments,
-    },
-  });
-});
+exports.getAllProducts = getAllDocuments(Product);
 
 /**
  * Middleware function used to create a new product document entry based on the user information passed into the body request object.
- * @param {object} req expects a request object
- * @param {object} res expects a response object
- * @param {function} next expects a function that will be used to  navigate to the next middleware
  */
-exports.createProduct = catchAsync(async (req, res, next) => {
-  //1 Create and insert into the Db de mongodb document
-  const productDocument = await Product.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: { productDocument },
-  });
-});
-
+exports.createProduct = createDocument(Product);
 /**
  * Middleware function used to delete  product document entry based on a user id
- * @param {object} req expects a request object
- * @param {object} res expects a response object
- * @param {function} next expects a function that will be used to  navigate to the next middleware
  */
-exports.deleteProduct = catchAsync(async (req, res, next) => {
-  const product = await Product.findOneAndDelete(req.params.id);
-
-  res.status(204).json({
-    status: 'success',
-    data: product,
-  });
-});
+exports.deleteProduct = deleteDocument(Product);
 
 /**
  * Middleware function used to update product document entry based on a user id and its field passed into the request body.
- * @param {object} req expects a request object
- * @param {object} res expects a response object
- * @param {function} next expects a function that will be used to  navigate to the next middleware
  */
-exports.updateProduct = catchAsync(async (req, res, next) => {
-  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    //returned document will be the newly updated one
-    new: true,
-    //each time a document is updated, the validators within the schema will run again
-    runValidators: true,
-  });
-
-  res.status(200).json({
-    status: 'success',
-    data: { product },
-  });
-});
+exports.updateProduct = updateDocument(Product);
